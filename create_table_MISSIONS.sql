ab9dbf91e825c13ace6af93966564063fdb9609a
rcook4 <36301073+rcook4@users.noreply.github.com>
2019-06-07 22:12:24 -0700

    Add files via upload

---
+++ create_table_MISSIONS.sql
@@ -0,0 +1,32 @@
+CREATE TABLE MISSIONS
+WITH (
+       format = 'TEXTFILE'
+      ,field_delimiter = ','
+      ,external_location = 's3://cs598project/answers/MISSIONS/'
+      ,partitioned_by = ARRAY['LAND_DAY']
+      ,bucketed_by = ARRAY['LAND_MONTH']
+      ,bucket_count = 12
+)
+AS
+SELECT
+       2008 AS YEAR
+      ,concat(LAUNCH, ' => ', LANDING.RENDEZVOUS, ' => ', LAND, ', ', date_format(LAUNCH_EARLIEST, '%d/%m/%Y')) AS NAME
+      ,LAUNCH
+      ,LAUNCH_EARLIEST
+      ,LAUNCH_LATEST
+      ,LANDING.RENDEZVOUS
+      ,LAND
+      ,LAND_EARLIEST
+      ,LAND_LATEST
+      ,month(LAUNCH_EARLIEST) AS LAND_MONTH
+      ,day(LAUNCH_EARLIEST) AS LAND_DAY
+FROM
+(
+      select distinct origin as LAUNCH, dest as RENDEZVOUS, date_add('second', 0, flight_ts) as LAUNCH_EARLIEST, date_add('second', 0+43199, flight_ts) as LAUNCH_LATEST from completed where year(flight_ts) = 2008 and hour(crsdep_ts) < 12
+) AS LAUNCHING
+JOIN
+(
+      select distinct origin as RENDEZVOUS, dest as LAND, date_add('second', 43200, flight_ts) as LAND_EARLIEST, date_add('second', 43200+43199, flight_ts) as LAND_LATEST from completed where year(flight_ts) = 2008 and hour(crsarr_ts) >= 12
+) as LANDING
+ON LAUNCHING.RENDEZVOUS = LANDING.RENDEZVOUS
+AND date_diff('day', LAUNCH_EARLIEST, LAND_EARLIEST) = 2;
