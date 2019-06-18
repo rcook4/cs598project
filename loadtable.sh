436ad26eede31f74702bcd522f927c73f2574a57
rcook4 <36301073+rcook4@users.noreply.github.com>
2019-06-18 16:28:18 -0700

    Update loadtable.sh

--- loadtable.sh
+++ loadtable.sh
@@ -1,19 +1,13 @@
+sudo chown root:ec2-user -R /wip/answers
+sudo chmod 777 -R /wip/answers
+sudo mkdir -p /wip/answers/$1/
 sudo rm -rf /wip/answers/$1/*
-cd /wip/answers/$1/
-aws s3 sync s3://cs598project/answers/$1 .
-sudo chown root:ec2-user -R .
-sudo chmod 777 -R .
+aws s3 sync s3://cs598project/answers/$1 /wip/answers/$1/
 gunzip *.gz
 sudo find . -type f -name "2019*" -and ! -name "*.gz" -print0 | xargs -0 -I{} sudo split -l 25 {}
-sudo chown root:ec2-user -R /wip/
-sudo chmod 777 -R /wip/
-if [ ! -f ./xaa ]; then
-    sudo find . -type f -name "2019*" -and ! -name "*.gz" -print0 | xargs -0 -I{} sudo cp {} ./xaa
-    sudo chown root:ec2-user -R ./xaa
-    sudo chmod 777 -R ./xaa
+if [ ! -f /wip/answers/$1/xaa ]; then
+    sudo find /wip/answers/$1/ -type f -name "2019*" -and ! -name "*.gz" -print0 | xargs -0 -I{} sudo cp {} /wip/answers/$1/xaa
+    sudo chown root:ec2-user -R /wip/answers/$1/xaa
+    sudo chmod 777 -R /wip/answers/$1/xaa
 fi
-sudo find . -type f -name "x*" -and ! -name "*.json" -print0 | xargs -0 -I{} /wip/answers/loadbatch.sh $1 {}
-fi
-sudo chown root:ec2-user -R /wip/
-sudo chmod 777 -R /wip/
-sudo find /wip/answers/$1/. -type f -name "x*" -and ! -name "*.json" -print0 | xargs -0 -I{} /wip/answers/loadbatch.sh $1 {}
+sudo find /wip/answers/$1/ -type f -name "x*" -and ! -name "*.json" -print0 | xargs -0 -I{} /wip/answers/loadbatch.sh $1 {}
