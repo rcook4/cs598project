494be4ddf7dd8f93be3fb81949e54f7bb03e8a6f
rcook4 <36301073+rcook4@users.noreply.github.com>
2019-06-18 16:49:42 -0700

    Update loadtable.sh

--- loadtable.sh
+++ loadtable.sh
@@ -4,10 +4,9 @@ sudo mkdir -p /wip/answers/$1/
 sudo rm -rf /wip/answers/$1/*
 aws s3 sync s3://cs598project/answers/$1 /wip/answers/$1/
 gunzip *.gz
-sudo find . -type f -name "2019*" -and ! -name "*.gz" -print0 | xargs -0 -I{} sudo split -l 25 {}
-if [ ! -f /wip/answers/$1/xaa ]; then
-    sudo find /wip/answers/$1/ -type f -name "2019*" -and ! -name "*.gz" -print0 | xargs -0 -I{} sudo cp {} /wip/answers/$1/xaa
-    sudo chown root:ec2-user -R /wip/answers/$1/xaa
-    sudo chmod 777 -R /wip/answers/$1/xaa
+sudo find . -type f -name "2019*" -and ! -name "*.gz" -print0 | xargs -0 -I{} sudo split -l 25 {} {}. -a 5
+batchcount=`ls -1 2019*.a* 2>/dev/null | wc -l`
+if [ $batchcount -eq 0 ]; then
+    sudo find /wip/answers/$1/ -type f -name "2019*" -and ! -name "*.gz" -print0 | xargs -0 -I{} sudo cp {} /wip/answers/$1/{}.aaaaa
 fi
-sudo find /wip/answers/$1/ -type f -name "x*" -and ! -name "*.json" -print0 | xargs -0 -I{} /wip/answers/loadbatch.sh $1 {}
+sudo find /wip/answers/$1/ -type f -name "2019*.a*" -and ! -name "*.json" -print0 | xargs -0 -I{} /wip/answers/loadbatch.sh $1 {}
