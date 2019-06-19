064699ef9f051e31df0fc9e7bc7d60854557f25a
rcook4 <36301073+rcook4@users.noreply.github.com>
2019-06-18 17:41:49 -0700

    Update g2q4.sql

--- g2q4.sql
+++ g2q4.sql
@@ -1,4 +1,4 @@
-/*runtime evidence*/
+/*timings*/
 CREATE TABLE g2q4
 WITH
 (
@@ -8,10 +8,12 @@ WITH
 AS
 SELECT concat('(', origin, '=>', dest, ')') AS flighthop, origin AS airport, dest AS destination, ROUND(1.0*SUM(date_diff('minute', crsarr_ts, arr_ts))/COUNT(*), 2) AS flighthop_mean_arrival_delay_minutes
 FROM completedflights
+/*
 WHERE origin = 'CMI' AND dest = 'ORD'
 OR origin = 'IND' AND dest = 'CMH'
 OR origin = 'DFW' AND dest = 'IAH'
 OR origin = 'LAX' AND dest = 'SFO'
 OR origin = 'JFK' AND dest = 'LAX'
 OR origin = 'ATL' AND dest = 'PHX'
+*/
 GROUP BY 2, 3;
