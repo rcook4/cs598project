1f1f29bd44335c9632d1ee064f2f09700180d323
rcook4 <36301073+rcook4@users.noreply.github.com>
2019-06-18 17:45:32 -0700

    Update loadanswers.sh

--- loadanswers.sh
+++ loadanswers.sh
@@ -1,6 +1,6 @@
 echo "start timings... $(date)"
-/wip/answers/loadtable.sh g2q1
-/wip/answers/loadtable.sh g2q2
-/wip/answers/loadtable.sh g2q3
-/wip/answers/loadtable.sh g2q4
+/wip/answers/loadtable.sh g2q1 >/dev/null 2>&1
+/wip/answers/loadtable.sh g2q2 >/dev/null 2>&1
+/wip/answers/loadtable.sh g2q3 >/dev/null 2>&1
+/wip/answers/loadtable.sh g2q4 >/dev/null 2>&1
 echo "stop timings... $(date)"
