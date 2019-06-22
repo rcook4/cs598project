6e7a3cc9fe0a2247c0535a7329a6ccea5e59354f
rcook4 <36301073+rcook4@users.noreply.github.com>
2019-06-21 22:28:54 -0700

    Update g2q3.sql

--- g2q3.sql
+++ g2q3.sql
@@ -9,7 +9,7 @@ AS
 SELECT concat(origin, '=>', dest) AS flighthop, origin AS airport, dest AS destination, uniquecarrier AS airline, fifteen_minutes_late_percentage, arrival_performance_rank AS flighthop_airline_best_arrival_performance_rank
 FROM
 (
-      SELECT origin, dest, uniquecarrier, DENSE_RANK() OVER (PARTITION BY origin, dest ORDER BY fifteen_minutes_late_percentage ASC) as arrival_performance_rank, fifteen_minutes_late_percentage
+      SELECT origin, dest, uniquecarrier, RANK() OVER (PARTITION BY origin, dest ORDER BY fifteen_minutes_late_percentage, uniquecarrier) as arrival_performance_rank, fifteen_minutes_late_percentage
       FROM
       (
             SELECT origin, dest, uniquecarrier, ROUND(100.0*SUM(arrdel15)/COUNT(*), 2) AS fifteen_minutes_late_percentage
