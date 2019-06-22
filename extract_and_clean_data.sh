b09e89ff858eb7176849d481cc13ae5a226a6e1e
rcook4 <36301073+rcook4@users.noreply.github.com>
2019-06-22 16:43:05 -0700

    Update extract_and_clean_data.sh

--- extract_and_clean_data.sh
+++ extract_and_clean_data.sh
@@ -4,13 +4,14 @@ sudo mount /dev/xvdb /snapshot >/dev/null 2>&1
 sudo mkdir -p /data/raw >/dev/null 2>&1
 sudo chown root:ec2-user -R /data/ >/dev/null 2>&1
 sudo chmod 777 -R /data/ >/dev/null 2>&1
-[ "$HOSTNAME" == "ip-172-31-46-45" ] && sudo find /snapshot/aviation/airline_ontime/. -type f -name "*1988*.zip" -or -name "*1989*.zip" -or -name "*1990*.zip" -or -name "*1991*.zip" -or -name "*1992*.zip" -print0 | xargs -0 -I{} sudo unzip {} -x readme.html -d /data/raw >/dev/null 2>&1
-[ "$HOSTNAME" == "ip-172-31-33-86" ] && sudo find /snapshot/aviation/airline_ontime/. -type f -name "*1993*.zip" -or -name "*1994*.zip" -or -name "*1995*.zip" -or -name "*1996*.zip" -or -name "*1997*.zip" -print0 | xargs -0 -I{} sudo unzip {} -x readme.html -d /data/raw >/dev/null 2>&1
-[ "$HOSTNAME" == "ip-172-31-37-87" ] && sudo find /snapshot/aviation/airline_ontime/. -type f -name "*1998*.zip" -or -name "*1999*.zip" -or -name "*2000*.zip" -or -name "*2001*.zip" -or -name "*2002*.zip" -or -name "*2008*.zip" -print0 | xargs -0 -I{} sudo unzip {} -x readme.html -d /data/raw >/dev/null 2>&1
-[ "$HOSTNAME" == "ip-172-31-44-141" ] && sudo find /snapshot/aviation/airline_ontime/. -type f -name "*2003*.zip" -or -name "*2004*.zip" -or -name "*2005*.zip" -or -name "*2006*.zip" -or -name "*2007*.zip" -print0 | xargs -0 -I{} sudo unzip {} -x readme.html -d /data/raw >/dev/null 2>&1
+[ "$HOSTNAME" == "ip-172-31-46-45" ] && sudo find /snapshot/aviation/airline_ontime/. -type f -name "*9_*.zip" -or -name "*0_*.zip" -or -name "*1_*.zip" -print0 | xargs -0 -I{} sudo unzip {} -x readme.html -d /data/raw >/dev/null 2>&1
+[ "$HOSTNAME" == "ip-172-31-33-86" ] && sudo find /snapshot/aviation/airline_ontime/. -type f -name "*2_*.zip" -or -name "*3_*.zip" -or -name "*4_*.zip" -print0 | xargs -0 -I{} sudo unzip {} -x readme.html -d /data/raw >/dev/null 2>&1
+[ "$HOSTNAME" == "ip-172-31-37-87" ] && sudo find /snapshot/aviation/airline_ontime/. -type f -name "*5_*.zip" -or -name "*6_*.zip" -print0 | xargs -0 -I{} sudo unzip {} -x readme.html -d /data/raw >/dev/null 2>&1
+[ "$HOSTNAME" == "ip-172-31-44-141" ] && sudo find /snapshot/aviation/airline_ontime/. -type f -name "*7_*.zip" -or -name "*8_*.zip" -print0 | xargs -0 -I{} sudo unzip {} -x readme.html -d /data/raw >/dev/null 2>&1
 sudo chown root:ec2-user -R /data/raw >/dev/null 2>&1
 sudo chmod 777 -R /data/raw >/dev/null 2>&1
 sudo find /data/raw/ -name '*.csv' -print0 | xargs -0 -I{} sudo sed -i 's/, /-/g' {} >/dev/null 2>&1
 sudo find /data/raw/ -name '*.csv' -print0 | xargs -0 -I{} sudo sed -i 's/\"//g' {} >/dev/null 2>&1
+sudo gzip /data/raw/*.csv
 aws s3 sync /data/raw s3://cs598project/data/raw/scrubbed >/dev/null 2>&1
 echo "end timings... $(date)"
