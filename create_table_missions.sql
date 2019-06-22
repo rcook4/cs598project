279eaa439d1470e9ef34c0f450303500181e9427
rcook4 <36301073+rcook4@users.noreply.github.com>
2019-06-22 10:44:12 -0700

    Update create_table_missions.sql

--- create_table_missions.sql
+++ create_table_missions.sql
@@ -11,23 +11,23 @@ WITH (
 AS
 SELECT
        2008 AS YEAR
-      ,concat(LAUNCH, ' => ', LANDING.RENDEZVOUS, ' => ', LAND, ' (', date_format(LAUNCH_EARLIEST, '%d/%m/%Y'), ')') AS NAME
+      ,concat(LAUNCH, ' => ', LANDING.RENDEZVOUS, ' => ', LAND, ' (', date_format(LAUNCH_EARLIEST_DEPARTURE, '%d/%m/%Y'), ')') AS NAME
       ,LAUNCH
-      ,LAUNCH_EARLIEST
-      ,LAUNCH_LATEST
+      ,LAUNCH_EARLIEST_DEPARTURE
+      ,LAUNCH_LATEST_DEPARTURE
       ,LANDING.RENDEZVOUS
       ,LAND
-      ,LAND_EARLIEST
-      ,LAND_LATEST
-      ,month(LAUNCH_EARLIEST) AS LAUNCH_MONTH
-      ,day(LAUNCH_EARLIEST) AS LAUNCH_DAY
+      ,LAND_EARLIEST_DEPARTURE
+      ,LAND_LATEST_DEPARTURE
+      ,month(LAUNCH_EARLIEST_DEPARTURE) AS LAUNCH_MONTH
+      ,day(LAUNCH_EARLIEST_DEPARTURE) AS LAUNCH_DAY
 FROM
 (
-      select distinct origin as LAUNCH, dest as RENDEZVOUS, date_add('second', 0, flight_ts) as LAUNCH_EARLIEST, date_add('second', 0+43199, flight_ts) as LAUNCH_LATEST from completedflights where year(flight_ts) = 2008 and hour(crsdep_ts) < 12
+      select distinct origin as LAUNCH, dest as RENDEZVOUS, date_add('second', 0, flight_ts) as LAUNCH_EARLIEST_DEPARTURE, date_add('second', 0+43199, flight_ts) as LAUNCH_LATEST_DEPARTURE from completedflights where year(flight_ts) = 2008 and hour(crsdep_ts) < 12
 ) AS LAUNCHING
 JOIN
 (
-      select distinct origin as RENDEZVOUS, dest as LAND, date_add('second', 43200, flight_ts) as LAND_EARLIEST, date_add('second', 43200+43199, flight_ts) as LAND_LATEST from completedflights where year(flight_ts) = 2008 and hour(crsarr_ts) >= 12
+      select distinct origin as RENDEZVOUS, dest as LAND, date_add('second', 43200, flight_ts) as LAND_EARLIEST_DEPARTURE, date_add('second', 43200+43199, flight_ts) as LAND_LATEST_DEPARTURE from completedflights where year(flight_ts) = 2008 and hour(crsarr_ts) >= 12
 ) as LANDING
 ON LAUNCHING.RENDEZVOUS = LANDING.RENDEZVOUS
-AND date_diff('day', LAUNCH_EARLIEST, LAND_EARLIEST) = 2;
+AND date_diff('day', LAUNCH_EARLIEST_DEPARTURE, LAND_EARLIEST_DEPARTURE) = 2;
