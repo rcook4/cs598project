286ef71de4750bbb19b5025acdd4959e71819860
rcook4 <36301073+rcook4@users.noreply.github.com>
2019-06-21 20:14:24 -0700

    Update loadanswers.sh

--- loadanswers.sh
+++ loadanswers.sh
@@ -8,9 +8,9 @@ aws dynamodb delete-table --table-name g2q3 >/dev/null 2>&1
 aws dynamodb delete-table --table-name g2q4 >/dev/null 2>&1
 echo "sleep for dynamodb delete tables..."
 sleep 5
-aws dynamodb create-table --table-name g1q1 --attribute-definitions AttributeName=airport_most_popular_rank,AttributeType=S --key-schema AttributeName=airport_most_popular_rank --billing-mode PAY_PER_REQUEST >/dev/null 2>&1
-aws dynamodb create-table --table-name g1q2 --attribute-definitions AttributeName=airline_best_arrival_performance_rank,AttributeType=S --key-schema AttributeName=airline_best_arrival_performance_rank --billing-mode PAY_PER_REQUEST >/dev/null 2>&1
-aws dynamodb create-table --table-name g1q3 --attribute-definitions AttributeName=dayofweek_best_arrival_performance_rank,AttributeType=S --key-schema AttributeName=dayofweek_best_arrival_performance_rank --billing-mode PAY_PER_REQUEST >/dev/null 2>&1
+aws dynamodb create-table --table-name g1q1 --attribute-definitions AttributeName=airport,AttributeType=S --key-schema AttributeName=airport --billing-mode PAY_PER_REQUEST >/dev/null 2>&1
+aws dynamodb create-table --table-name g1q2 --attribute-definitions AttributeName=airline,AttributeType=S --key-schema AttributeName=airline --billing-mode PAY_PER_REQUEST >/dev/null 2>&1
+aws dynamodb create-table --table-name g1q3 --attribute-definitions AttributeName=day_of_week,AttributeType=S --key-schema AttributeName=day_of_week --billing-mode PAY_PER_REQUEST >/dev/null 2>&1
 aws dynamodb create-table --table-name g2q1 --attribute-definitions AttributeName=airport,AttributeType=S AttributeName=airline,AttributeType=S --key-schema AttributeName=airport,KeyType=HASH AttributeName=airline,KeyType=RANGE --billing-mode PAY_PER_REQUEST >/dev/null 2>&1
 aws dynamodb create-table --table-name g2q2 --attribute-definitions AttributeName=airport,AttributeType=S AttributeName=destination,AttributeType=S --key-schema AttributeName=airport,KeyType=HASH AttributeName=destination,KeyType=RANGE --billing-mode PAY_PER_REQUEST >/dev/null 2>&1
 aws dynamodb create-table --table-name g2q3 --attribute-definitions AttributeName=flighthop,AttributeType=S AttributeName=airline,AttributeType=S --key-schema AttributeName=flighthop,KeyType=HASH AttributeName=airline,KeyType=RANGE --billing-mode PAY_PER_REQUEST >/dev/null 2>&1
