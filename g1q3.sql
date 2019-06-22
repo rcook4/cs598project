8f8b7567b2ff93d9b9c48a55c7a9ff8cb25d04ad
rcook4 <36301073+rcook4@users.noreply.github.com>
2019-06-21 20:32:45 -0700

    Update g1q3.sql

--- g1q3.sql
+++ g1q3.sql
@@ -6,10 +6,10 @@ WITH
      external_location = 's3://cs598project/answers/g1q3/'
 ) 
 AS
-SELECT day_of_week, fifteen_minutes_late_percentage, RANK() OVER (ORDER BY fifteen_minutes_late_percentage ASC) AS dayofweek_best_arrival_performance_rank
+SELECT dayofweek, fifteen_minutes_late_percentage, RANK() OVER (ORDER BY fifteen_minutes_late_percentage ASC) AS dayofweek_best_arrival_performance_rank
 FROM
 (
-      select date_format(flight_ts, '%a') as day_of_week, ROUND(100.0*SUM(arrdel15)/COUNT(*), 2) AS fifteen_minutes_late_percentage
+      select date_format(flight_ts, '%a') as dayofweek, ROUND(100.0*SUM(arrdel15)/COUNT(*), 2) AS fifteen_minutes_late_percentage
       from completedflights
       group by 1
       order by 2 ASC
