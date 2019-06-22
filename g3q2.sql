22351275dfe8df7d3340b3842354eddda444da46
rcook4 <36301073+rcook4@users.noreply.github.com>
2019-06-22 10:56:35 -0700

    Update g3q2.sql

--- g3q2.sql
+++ g3q2.sql
@@ -9,33 +9,33 @@ WITH
 ) 
 AS
 SELECT
-       NAME as mission
-      ,launching_flight
-      ,actual_launch
-      ,landing_flight
-      ,actual_landing
-      ,total_mission_delayed_minutes
+       mission
+      ,launch_flight
+      ,scheduled_launch
+      ,land_flight
+      ,scheduled_land
+      ,delay
       ,LAUNCH_DAY
 FROM
 (
       SELECT
-             NAME
-            ,launching_flight
-            ,actual_launch
-            ,landing_flight
-            ,actual_landing
-            ,total_mission_delayed_minutes
-            ,RANK() OVER (PARTITION BY NAME ORDER BY total_mission_delayed_minutes ASC) AS route_best_match_rank
+             mission
+            ,launch_flight
+            ,scheduled_launch
+            ,land_flight
+            ,scheduled_land
+            ,delay
+            ,RANK() OVER (PARTITION BY mission ORDER BY delay ASC) AS route_best_match_rank
             ,LAUNCH_DAY
       FROM
       (
             SELECT
-                   MISSIONS.NAME
-                  ,concat(LAUNCHING.uniquecarrier, ' ', LAUNCHING.flightnum) AS launching_flight
-                  ,LAUNCHING.dep_ts AS actual_launch
-                  ,concat(LANDING.uniquecarrier, ' ', LANDING.flightnum) as landing_flight
-                  ,LANDING.arr_ts AS actual_landing
-                  ,(date_diff('minute', LAUNCHING.crsarr_ts, LAUNCHING.arr_ts) + date_diff('minute', LANDING.crsarr_ts, LANDING.arr_ts)) as total_mission_delayed_minutes
+                   MISSIONS.NAME as mission
+                  ,concat(LAUNCHING.uniquecarrier, ' ', LAUNCHING.flightnum) AS launch_flight
+                  ,date_format(LAUNCHING.crsdep_ts, '%d/%m/%Y %H:%i') AS scheduled_launch
+                  ,concat(LANDING.uniquecarrier, ' ', LANDING.flightnum) as land_flight
+                  ,date_format(LANDING.crsarr_ts, '%d/%m/%Y %H:%i') AS scheduled_land
+                  ,(date_diff('minute', LAUNCHING.crsarr_ts, LAUNCHING.arr_ts) + date_diff('minute', LANDING.crsarr_ts, LANDING.arr_ts)) as delay
                   ,LAUNCH_DAY
             FROM MISSIONS
             JOIN completedflights AS LAUNCHING
@@ -43,18 +43,18 @@ FROM
                   AND LAUNCHING.year = MISSIONS.YEAR
                   AND LAUNCHING.origin = MISSIONS.LAUNCH
                   AND LAUNCHING.dest = MISSIONS.RENDEZVOUS
-                  AND LAUNCHING.dep_ts BETWEEN MISSIONS.LAUNCH_EARLIEST AND MISSIONS.LAUNCH_LATEST
-                  AND LAUNCHING.crsdep_ts BETWEEN MISSIONS.LAUNCH_EARLIEST AND MISSIONS.LAUNCH_LATEST
+                  AND LAUNCHING.dep_ts BETWEEN MISSIONS.LAUNCH_EARLIEST_DEPARTURE AND MISSIONS.LAUNCH_LATEST_DEPARTURE
+                  AND LAUNCHING.crsdep_ts BETWEEN MISSIONS.LAUNCH_EARLIEST_DEPARTURE AND MISSIONS.LAUNCH_LATEST_DEPARTURE
                   AND day(LAUNCHING.dep_ts) = MISSIONS.LAUNCH_DAY
             JOIN completedflights AS LANDING
                   ON  LANDING.year = 2008
                   AND LANDING.year = MISSIONS.YEAR
                   AND LANDING.origin = MISSIONS.RENDEZVOUS
                   AND LANDING.dest = MISSIONS.LAND
-                  AND LANDING.arr_ts BETWEEN MISSIONS.LAND_EARLIEST AND MISSIONS.LAND_LATEST
-                  AND LANDING.crsarr_ts BETWEEN MISSIONS.LAND_EARLIEST AND MISSIONS.LAND_LATEST
+                  AND LANDING.dep_ts BETWEEN MISSIONS.LAND_EARLIEST_DEPARTURE AND MISSIONS.LAND_LATEST_DEPARTURE
+                  AND LANDING.crsdep_ts BETWEEN MISSIONS.LAND_EARLIEST_DEPARTURE AND MISSIONS.LAND_LATEST_DEPARTURE
                   AND day(date_add('day', -2, LANDING.arr_ts)) = MISSIONS.LAUNCH_DAY
-/*            WHERE NAME IN ('CMI => ORD => LAX (04/03/2008)','JAX => DFW => CRP (09/09/2008)','SLC => BFL => LAX (01/04/2008)','LAX => SFO => PHX (12/07/2008)','DFW => ORD => DFW (10/06/2008)','LAX => ORD => JFK (01/01/2008)') */
+/*            WHERE MISSIONS.NAME IN ('CMI => ORD => LAX (04/03/2008)','JAX => DFW => CRP (09/09/2008)','SLC => BFL => LAX (01/04/2008)','LAX => SFO => PHX (12/07/2008)','DFW => ORD => DFW (10/06/2008)','LAX => ORD => JFK (01/01/2008)') */
       ) AS MISSION_ROUTES
 ) AS MISSION_RESULT
 WHERE route_best_match_rank = 1;
