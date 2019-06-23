55eed0d81592554c5881b6e6fa42aae700cdf283
rcook4 <36301073+rcook4@users.noreply.github.com>
2019-06-22 18:11:54 -0700

    Update loadanswers.sh

--- loadanswers.sh
+++ loadanswers.sh
@@ -15,11 +15,11 @@ sleep 15
 [ "$HOSTNAME" == "ip-172-31-33-86" ] && aws dynamodb create-table --table-name g2q3 --attribute-definitions AttributeName=flighthop,AttributeType=S AttributeName=flighthop_airline_best_arrival_performance_rank,AttributeType=N --key-schema AttributeName=flighthop,KeyType=HASH AttributeName=flighthop_airline_best_arrival_performance_rank,KeyType=RANGE --billing-mode PAY_PER_REQUEST >/dev/null 2>&1
 [ "$HOSTNAME" == "ip-172-31-37-87" ] && aws dynamodb create-table --table-name g2q4 --attribute-definitions AttributeName=flighthop,AttributeType=S --key-schema AttributeName=flighthop,KeyType=HASH --billing-mode PAY_PER_REQUEST >/dev/null 2>&1
 sleep 30
-[ "$HOSTNAME" == "ip-172-31-44-141" ] && /wip/answers/loadtable.sh g1q1 >/dev/null 2>&1
-[ "$HOSTNAME" == "ip-172-31-44-141" ] && /wip/answers/loadtable.sh g1q2 >/dev/null 2>&1
-[ "$HOSTNAME" == "ip-172-31-44-141" ] && /wip/answers/loadtable.sh g1q3 >/dev/null 2>&1
-[ "$HOSTNAME" == "ip-172-31-46-45" ] && /wip/answers/loadtable.sh g2q1 >/dev/null 2>&1
-[ "$HOSTNAME" == "ip-172-31-46-45" ] && /wip/answers/loadtable.sh g2q2 >/dev/null 2>&1
-[ "$HOSTNAME" == "ip-172-31-33-86" ] && /wip/answers/loadtable.sh g2q3 >/dev/null 2>&1
-[ "$HOSTNAME" == "ip-172-31-37-87" ] && /wip/answers/loadtable.sh g2q4 >/dev/null 2>&1
+[ "$HOSTNAME" == "ip-172-31-44-141" ] && /wip/loadtable.sh g1q1 >/dev/null 2>&1
+[ "$HOSTNAME" == "ip-172-31-44-141" ] && /wip/loadtable.sh g1q2 >/dev/null 2>&1
+[ "$HOSTNAME" == "ip-172-31-44-141" ] && /wip/loadtable.sh g1q3 >/dev/null 2>&1
+[ "$HOSTNAME" == "ip-172-31-46-45" ] && /wip/loadtable.sh g2q1 >/dev/null 2>&1
+[ "$HOSTNAME" == "ip-172-31-46-45" ] && /wip/loadtable.sh g2q2 >/dev/null 2>&1
+[ "$HOSTNAME" == "ip-172-31-33-86" ] && /wip/loadtable.sh g2q3 >/dev/null 2>&1
+[ "$HOSTNAME" == "ip-172-31-37-87" ] && /wip/loadtable.sh g2q4 >/dev/null 2>&1
 echo "stop timings... $(date)"
